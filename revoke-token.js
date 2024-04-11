af98106195ba2886040a058b2ba19e7c9c7a4e7a
Boris J. Guzitajevas <borisrunfast@gmail.com>
2024-04-11 19:30:54 +0200

    yes

--- revoke-token.js
+++ revoke-token.js
@@ -5,20 +5,17 @@ const userToken = urlParams.get('access_token')
 console.log(userToken)
 
 async function getUserData(accessToken) {
-    try {
-        console.log(accessToken)
-        const response = await fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
-            headers: {
-                "Authorization": `Bearer ${accessToken}`
-            }
-        })
-        const data = await response.json()
-        return data
-    } catch (err) {
-        console.log(err)
-    }
+    fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: {
+            "Authorization": `Bearer ${accessToken}`
+        }
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+    })
   }
 
-  const data = getUserData(userToken)
-  console.log(data)
+  getUserData(userToken)
+
 
