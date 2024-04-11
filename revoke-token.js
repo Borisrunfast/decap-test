7bb17908aa0f4117f7c3d8441b4140b219f3829a
Boris J. Guzitajevas <borisrunfast@gmail.com>
2024-04-11 18:47:32 +0200

    get user data

--- revoke-token.js
+++ revoke-token.js
@@ -3,27 +3,15 @@ const urlParams = new URLSearchParams(fragmentStr)
 const userToken = urlParams.get('access_token')
 
 
-function revokeAccess(accessToken) {
-    // Google's OAuth 2.0 endpoint for revoking access tokens.
-    let revokeTokenEndpoint = 'https://oauth2.googleapis.com/revoke';
-  
-    // Create <form> element to use to POST data to the OAuth 2.0 endpoint.
-    let form = document.createElement('form');
-    form.setAttribute('method', 'post');
-    form.setAttribute('action', revokeTokenEndpoint);
-  
-    // Add access token to the form so it is set as value of 'token' parameter.
-    // This corresponds to the sample curl request, where the URL is:
-    //      https://oauth2.googleapis.com/revoke?token={token}
-    let tokenField = document.createElement('input');
-    tokenField.setAttribute('type', 'hidden');
-    tokenField.setAttribute('name', 'token');
-    tokenField.setAttribute('value', accessToken);
-    form.appendChild(tokenField);
-  
-    // Add form to page and submit it to actually revoke the token.
-    document.body.appendChild(form);
-    form.submit();
+async function getUserData(accessToken) {
+    const response = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        }
+    })
+    const data = await response.json()
+    return data
   }
 
-  console.log(revokeAccess(userToken))
+  console.log(getUserData(userToken))
+
